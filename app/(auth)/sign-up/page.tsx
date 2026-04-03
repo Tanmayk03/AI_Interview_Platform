2898df07f4f47803c0da53ec5546fe806f1b2b20
Tanmay Kapoor <142162139+Tanmayk03@users.noreply.github.com>
2026-04-03 14:29:35 +0530

    Initial Commit

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import AuthForm from '../../../components/AuthForm'
-
 const page = () => {
-  return <AuthForm type="signup" />
+  return (
+    <AuthForm type="sign-up" />
+  )
 }
 
 export default page
